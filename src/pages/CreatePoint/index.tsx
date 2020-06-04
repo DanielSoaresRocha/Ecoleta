6c6c382bc51c12f867b7d087addf1218554ba1a8
DanielSoaresRocha <legoliga123@gmail.com>
2020-06-04 14:27:52 -0300

    integrando o mapa do leaflet ao formulário

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
+import { Map, TileLayer, Marker } from 'react-leaflet'
 
 import './styles.css';
 
@@ -57,6 +58,14 @@ const CreatePoint = () => {
                         <h2>Endereço</h2>
                         <span>Selecione um endereço no mapa</span>
                     </legend>
+
+                    <Map center={[-5.9350376, -35.2478006]} zoom={15}>
+                        <TileLayer
+                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={[-5.9350376, -35.2478006]}></Marker>
+                    </Map>
 
                     <div className="field-group">
                         <div className="field">
